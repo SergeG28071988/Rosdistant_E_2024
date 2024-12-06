1453bd3627b7bdd1da8cadd6c87f3996c00fde70
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-12-06 16:53:55 +0300

    Переписали код

--- Theme_3/Zad_3_3.py
+++ Theme_3/Zad_3_3.py
@@ -1,10 +1,32 @@
-while True:
-    try:
-        number = int(input("Введите число: "))
+class EvenOddChecker:
+    def __init__(self):
+        self.welcome_message()
+
+    def welcome_message(self):
+        print("Добро пожаловать в программу проверки четности числа!")
+        print("Введите 'exit' для выхода.")
+
+    def input_number(self):
+        while True:
+            user_input = input("Введите число: ")
+            if user_input.lower() == 'exit':
+                print("Выход из программы. До свидания!")
+                break
+            try:
+                number = int(user_input)
+                self.check_even_odd(number)
+            except ValueError:
+                self.handle_error()
+
+    def check_even_odd(self, number):
         if number & 1 == 0:
             print("Четное")
         else:
             print("Нечетное")
-        break
-    except ValueError:
+
+    def handle_error(self):
         print("Ошибка: Введите целое число.")
+
+if __name__ == "__main__":
+    checker = EvenOddChecker()
+    checker.input_number()
