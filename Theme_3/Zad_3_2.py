02e303345cac7d3bce97f7b3e4cd4e6358efd8a3
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-12-13 09:06:33 +0300

    Изменили и доработали код к заданию из темы 3

--- Theme_3/Zad_3_2.py
+++ Theme_3/Zad_3_2.py
@@ -1,13 +1,40 @@
-while True:
-    try:
-        number = float(input("Введите число: "))
-        if number.is_integer():
-            if number % 2 == 0:
-                print("Четное")
-            else:
-                print("Нечетное")
+def main_menu():
+    print("Добро пожаловать в программу определения четности числа!")
+    print("1. Ввести число")
+    print("2. Выход")
+
+def check_even_odd(number):
+    if number.is_integer():
+        if number % 2 == 0:
+            return "Четное"
+        else:
+            return "Нечетное"
+    else:
+        return "Ошибка: Введите целое число."
+
+def get_number():
+    while True:
+        try:
+            user_input = input("Введите число: ")
+            number = float(user_input)
+            return number
+        except ValueError:
+            print("Ошибка: Введите корректное число. Например, 5 или 3.14.")
+
+def main():
+    while True:
+        main_menu()
+        choice = input("Выберите действие (1 или 2): ")
+        
+        if choice == '1':
+            number = get_number()
+            result = check_even_odd(number)
+            print(result)
+        elif choice == '2':
+            print("Спасибо за использование программы! До свидания!")
             break
         else:
-            print("Ошибка: Введите целое число.")
-    except ValueError:
-        print("Ошибка: Введите число.")
+            print("Ошибка: Пожалуйста, выберите 1 или 2.")
+
+if __name__ == "__main__":
+    main()
